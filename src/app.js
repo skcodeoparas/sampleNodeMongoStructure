427f935739cbd6ac8d6e6bfb31ab8b80507c5851
skcodeoparas <sushilcodingtest@outlook.com>
2022-08-23 21:49:37 +0530

    cors added

--- src/app.js
+++ src/app.js
@@ -1,12 +1,28 @@
-const express = require('express');
+const cors = require("cors");
+const express = require("express");
 const app = express();
-const Router = require('./router/main.router');
-const mongodb = require('./service/mongoCon.service')
+const Router = require("./router/main.router");
+const mongodb = require("./service/mongoCon.service");
 
-
+app.use(express.json());
+if (String(process.env.NODE_ENV) === "dev") {
+  const Whitelist = ["http://localhost:3000", "http://localhiost:4000"];
+  var corsOptions = {
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  };
+  app.use(cors(corsOptions));
+} else {
+  app.use(cors());
+}
 app.use(mongodb);
 app.use(Router);
 const port = process.env.port || 6000;
-app.listen(port, ()=>console.log(`server running in port ${port}`));
+app.listen(port, () => console.log(`server running in port ${port}`));
 
 module.exports = app;
