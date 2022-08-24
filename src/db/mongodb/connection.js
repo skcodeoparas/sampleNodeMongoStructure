aa982982caec587cf41663f1986875f95756e569
skcodeoparas <sushilcodingtest@outlook.com>
2022-08-24 12:39:29 +0530

    dbcon check

--- src/db/mongodb/connection.js
+++ src/db/mongodb/connection.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 
    async function connection(){
+    if(mongoose.connection.readyState === 0){
         try{
-            mongoose.connect(process.env.mongodburl );
-        }catch(e){
-            throw `there is an error : ${e}`
-        }
+                    mongoose.connect(process.env.mongodburl );
+                    console.log("connecting DB")
+                }catch(e){
+                    throw `there is an error : ${e}`
+                }
+    }else { return " connected" } 
     }
-
+console.log(mongoose.connection.readyState);
 module.exports = connection;
